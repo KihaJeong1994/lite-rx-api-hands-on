c8db0eec26d81ec6122998987a7dd3e2587a297a
KihaJeong1994 <jkha7371@gmail.com>
2022-08-16 15:37:20 +0900

    Part04Transform  Complete : map vs flatMap

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -15,21 +15,26 @@ public class Part04Transform {
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(user -> {
+			String username = user.getUsername().toUpperCase();
+			String firstname = user.getFirstname().toUpperCase();
+			String lastname = user.getLastname().toUpperCase();
+			return new User(username,firstname,lastname);
+		});
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(user -> capitalizeOne(Mono.just(user)));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(user -> asyncCapitalizeUser(user));
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
